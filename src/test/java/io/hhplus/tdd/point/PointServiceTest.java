555635eade6cc398436bed583634c242280cf993
yonghun <asqwklop12@gmail.com>
2025-05-19 22:36:45 +0900

    Test: 1번 포인트를 조회하는 경우 테스트

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,35 @@
+package io.hhplus.tdd.point;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PointServiceTest {
+
+  private UserPointTable userPoint;
+  private PointHistoryTable pointHistory;
+  private PointService pointService;
+
+  @BeforeEach
+  void init() {
+    //given
+    userPoint = mock(UserPointTable.class);
+    pointHistory = mock(PointHistoryTable.class);
+    pointService = new PointService(userPoint, pointHistory);
+  }
+
+  @Test
+  public void _1번_포인트를_조회하는경우() {
+    //given
+    when(userPoint.selectById(1L)).thenReturn(new UserPoint(1L, 1000L, 200L));
+    //when
+    UserPoint point = pointService.point(1L);
+    //then
+    assertThat(point.id()).isEqualTo(1L);
+  }
+}
