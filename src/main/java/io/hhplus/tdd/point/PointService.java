6aded6b23c987dbfc651bfd7d935712356c4ed6d
yonghun <asqwklop12@gmail.com>
2025-05-19 11:27:08 +0900

    feat: 테이블 등록 (TDD 준비완료)

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,10 +1,21 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
+
+  private final UserPointTable userPointTable;
+  private final PointHistoryTable pointHistoryTable;
+
+  public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
+    this.userPointTable = userPointTable;
+    this.pointHistoryTable = pointHistoryTable;
+  }
+
   public UserPoint point(long id) {
     return new UserPoint(0, 0, 0);
   }
