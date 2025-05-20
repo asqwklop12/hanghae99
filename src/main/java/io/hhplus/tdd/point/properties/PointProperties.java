670e5abbd5dded609113f3a0d64e5afd3774db71
yonghun <asqwklop12@gmail.com>
2025-05-21 02:04:16 +0900

    Refactor: setter 추가

--- src/main/java/io/hhplus/tdd/point/properties/PointProperties.java
+++ src/main/java/io/hhplus/tdd/point/properties/PointProperties.java
@@ -1,12 +1,14 @@
 package io.hhplus.tdd.point.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("point")
 @Getter
+@Setter
 public class PointProperties {
   private long availableCharge;
   private long max;
